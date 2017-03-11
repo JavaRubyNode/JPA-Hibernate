5201fa64124668994e57ac42f3b0cf62a7d91e16
JavaRubyNode <viniciusjavaruby@gmail.com>
2017-03-11 17:10:07 -0300

    novos metodos para a classe testes

--- jpa-seed-master/src/test/java/br/com/faculdadedelta/util/ProdutoTest.java
+++ jpa-seed-master/src/test/java/br/com/faculdadedelta/util/ProdutoTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.function.LongFunction;
 
 import br.edu.faculdadedelta.modelo.Produto;
 import br.edu.faculdadedelta.util.JpaUtil;
@@ -49,4 +50,46 @@ public class ProdutoTest {
 		assertTrue("encontrou varios produtos", listaProdutos.size() >= 10);
 	}
 
+	@Test
+	public void alterarProduto(){
+		salvarProduto();
+		TypedQuery<Produto> query = em.createQuery("SELECT p FROM Produto p", Produto.class).setMaxResults(1);
+		Produto produto = query.getSingleResult();
+		assertNotNull("encontrado um produto", produto);
+		Integer versao = produto.getVersion();
+		em.getTransaction().begin();
+		produto.setNome("vinicius");
+		produto = em.merge(produto);
+		em.getTransaction().commit();
+		assertNotEquals("versao incrementada", versao.intValue(),produto.getVersion().intValue());
+		
+		
+	}
+	
+	@Test
+	public void excluirProduto(){
+		salvarProduto();
+		TypedQuery<Long> query = em.createQuery("select max(p.id) from Produto p ",Long.class);
+		Long id = query.getSingleResult();
+		em.getTransaction().begin();
+		Produto produto = em.find(Produto.class, id);
+		em.remove(produto);
+		em.getTransaction().commit();
+		Produto excluido = em.find(Produto.class, id);
+		assertNull("nao encontrado ",excluido);
+		
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
